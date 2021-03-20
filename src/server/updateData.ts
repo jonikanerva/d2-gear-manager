3300142109b00ad92b538b22c664cf8f9479eda6
Joni Kanerva <jhonny@jhonny.org>
2021-03-20 17:53:58 +0200

    fetch all data locally for debugging

---
+++ src/server/updateData.ts
@@ -0,0 +1,47 @@
+import { promises as fs } from 'fs'
+import fetch from 'node-fetch'
+
+import { components } from '../bungie'
+import { config } from './config'
+
+type Manifest = components['schemas']['Destiny.Config.DestinyManifest']
+
+const getManifest = (): Promise<Manifest> =>
+  fetch('https://www.bungie.net/Platform/Destiny2/Manifest/', {
+    headers: { 'X-API-Key': config.bungieApiKey },
+  })
+    .then((res) => res.json())
+    .then((manifest) => manifest.Response)
+
+const getContent = (path: string) => {
+  console.log('Downloading: ', path)
+
+  return fetch(`https://www.bungie.net${path}`)
+    .then((res) => res.json())
+    .then((json) => JSON.stringify(json, null, 2))
+}
+
+const storeToFile = (filename: string, data: string): Promise<void> =>
+  fs.writeFile(`./data/${filename}.json`, data)
+
+const storePath = ({
+  name,
+  path,
+}: {
+  name: string
+  path: string
+}): Promise<void> => getContent(path).then((data) => storeToFile(name, data))
+
+const callInSequence = <T>(list: T[], fnc: (arg: T) => Promise<any>) =>
+  list.reduce((memo, item) => memo.then(() => fnc(item)), Promise.resolve())
+
+const dumpAll = () =>
+  getManifest()
+    .then((manifest) => manifest?.jsonWorldComponentContentPaths?.en || {})
+    .then((paths) =>
+      Object.entries(paths).map(([key, value]) => ({ name: key, path: value }))
+    )
+    .then((paths) => callInSequence(paths, storePath))
+    .catch((error) => console.error(error))
+
+dumpAll()
