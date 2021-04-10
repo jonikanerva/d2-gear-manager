e3ae243659e768aa4ab73c5953e2ef1ccfc2cd0e
Joni Kanerva <jhonny@jhonny.org>
2021-04-10 10:39:30 +0300

    let's try this again

--- src/modules/updateData.ts
+++ src/modules/updateData.ts
@@ -35,16 +35,18 @@ const storePath = ({
 const callInSequence = <T>(list: T[], fnc: (arg: T) => Promise<void>) =>
   list.reduce((memo, item) => memo.then(() => fnc(item)), Promise.resolve())
 
-export const downloadData = (): Promise<void> =>
-  getManifest()
-    .then((manifest) => manifest?.jsonWorldComponentContentPaths?.en || {})
-    .then((paths) =>
-      Object.entries(paths).map(([key, value]) => ({
-        name: key,
-        path: value,
-      }))
-    )
-    .then((paths) => callInSequence(paths, storePath))
-    .catch((error) => console.error(error))
+const isProduction = (): boolean => config.environment === 'production'
 
-downloadData()
+export const downloadData = (): Promise<void> =>
+  isProduction()
+    ? getManifest()
+        .then((manifest) => manifest?.jsonWorldComponentContentPaths?.en || {})
+        .then((paths) =>
+          Object.entries(paths).map(([key, value]) => ({
+            name: key,
+            path: value,
+          }))
+        )
+        .then((paths) => callInSequence(paths, storePath))
+        .catch((error) => console.error(error))
+    : Promise.resolve()
