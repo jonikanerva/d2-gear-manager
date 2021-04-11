86c9ed54bb2f9ac5544304bf2cc5ea338b3363ab
Joni Kanerva <jhonny@jhonny.org>
2021-04-11 08:53:19 +0300

    title

--- src/server/templates/html.tsx
+++ src/server/templates/html.tsx
@@ -15,7 +15,7 @@ const appConfig = {
 const html = (url: string): string => `<!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Destiny Gear Manager</title>
+  <title>Destiny GM</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
   <link rel="stylesheet" href="/${manifest['main.css'].replace(/^auto/, '')}" />
